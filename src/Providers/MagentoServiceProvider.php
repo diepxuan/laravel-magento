aef92c0d6ecd964ce9536fbb96bb4faa0d8fa45d
Tran Ngoc Duc <caothu91@gmail.com>
2024-05-07 02:21:22 +0000

    load base from core

--- src/Providers/MagentoServiceProvider.php
+++ src/Providers/MagentoServiceProvider.php
@@ -8,70 +8,24 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2024-05-06 18:36:37
+ * @lastupdate 2024-05-07 09:09:13
  */
 
 namespace Diepxuan\Magento\Providers;
 
-use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class MagentoServiceProvider extends ServiceProvider
 {
-    protected string $moduleName      = 'diepxuan/laravel-magento';
-    protected string $moduleNameLower = 'magento';
-
     /**
      * Bootstrap the application services.
      */
-    public function boot(): void
-    {
-        // $this->registerPublishing();
-
-        $this->registerCommands();
-        $this->registerCommandSchedules();
-        $this->registerTranslations();
-        $this->registerConfig();
-        $this->registerViews();
-        $this->loadMigrationsFrom(module_path($this->moduleName, 'database/migrations'));
-    }
+    public function boot(): void {}
 
     /**
      * Register the application services.
      */
     public function register(): void {}
-
-    /**
-     * Register translations.
-     */
-    public function registerTranslations(): void
-    {
-        $langPath = resource_path('lang/modules/' . $this->moduleNameLower);
-
-        if (is_dir($langPath)) {
-            $this->loadTranslationsFrom($langPath, $this->moduleNameLower);
-            $this->loadJsonTranslationsFrom($langPath);
-        } else {
-            $this->loadTranslationsFrom(module_path($this->moduleName, 'lang'), $this->moduleNameLower);
-            $this->loadJsonTranslationsFrom(module_path($this->moduleName, 'lang'));
-        }
-    }
-
-    /**
-     * Register views.
-     */
-    public function registerViews(): void
-    {
-        $viewPath   = resource_path('views/modules/' . $this->moduleNameLower);
-        $sourcePath = module_path($this->moduleName, 'resources/views');
-
-        $this->publishes([$sourcePath => $viewPath], ['views', $this->moduleNameLower . '-module-views']);
-
-        $this->loadViewsFrom(array_merge($this->getPublishableViewPaths(), [$sourcePath]), $this->moduleNameLower);
-
-        $componentNamespace = str_replace('/', '\\', $this->moduleName);
-        Blade::componentNamespace($componentNamespace, $this->moduleNameLower);
-    }
 
     /**
      * Get the services provided by the provider.
@@ -80,49 +34,6 @@ class MagentoServiceProvider extends ServiceProvider
      */
     public function provides(): array
     {
-        return ['magento'];
-    }
-
-    /**
-     * Register config.
-     */
-    protected function registerConfig(): void
-    {
-        $this->publishes([module_path($this->moduleName, 'config/config.php') => config_path($this->moduleNameLower . '.php')], 'config');
-        $this->mergeConfigFrom(module_path($this->moduleName, 'config/config.php'), $this->moduleNameLower);
-    }
-
-    /**
-     * Register commands in the format of Command::class.
-     */
-    protected function registerCommands(): void
-    {
-        // $this->commands([]);
-    }
-
-    /**
-     * Register command Schedules.
-     */
-    protected function registerCommandSchedules(): void
-    {
-        // $this->app->booted(function () {
-        //     $schedule = $this->app->make(Schedule::class);
-        //     $schedule->command('inspire')->hourly();
-        // });
-    }
-
-    /**
-     * @return array<string>
-     */
-    private function getPublishableViewPaths(): array
-    {
-        $paths = [];
-        foreach (config('view.paths') as $path) {
-            if (is_dir($path . '/modules/' . $this->moduleNameLower)) {
-                $paths[] = $path . '/modules/' . $this->moduleNameLower;
-            }
-        }
-
-        return $paths;
+        return [];
     }
 }
